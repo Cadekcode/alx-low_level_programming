1e970d72198616ef6dbce1a42fa3aa1770ac6196
cadekcode <adedejiakanni@gmail.com>
2023-03-16 16:41:02 -0700

    my first C code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - a C program that prints exactly
+ *
+ * Return:Always zero means succesful
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
