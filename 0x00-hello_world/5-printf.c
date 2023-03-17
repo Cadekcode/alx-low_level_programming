3377b9bf22e1f3ca126ad6f9cf8baa7aa35be7ec
cadekcode <adedejiakanni@gmail.com>
2023-03-16 22:29:08 -0700

    C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - a C program that prints exactly with proper grammar
  *
@@ -8,6 +7,6 @@
  */
 int main(void)
 { 
-	printf("w/ith proper grammar, but the outcome is a piece of art,\n")
-	return (0)
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
