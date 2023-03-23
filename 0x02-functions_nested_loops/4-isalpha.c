3fe757277116e75ff4abaa249e322d8f0430f8d5
cadekcode <adedejiakanni@gmail.com>
2023-03-22 20:56:04 -0700

    debugging

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,15 +2,15 @@
 
 /**
  * _isalpha - print letter, lowercase or uppercase.
- *
+ * @c: char to printed
  * Return: 1 if successful and 0 otherwise.
  */
 
 int _isalpha(int c)
 {
-	char c;
-	if ((c = 'a' && c <= 'z') || (c = 'A' && c<= 'Z'));
-	return (1);
-	else
-		return (0);
+	if (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	return (0);
+	}
 }
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,26 +2,25 @@
 
 /**
  * print_sign - print the sign of a number.
- *
+ * @n: the number to be printed
  * Return: 1 for positive, 0 for 0 and -1 for negative.
  */
 
 int print_sign(int n)
 {
-	int n;
-	if (n > 0);
+	if (n > 0)
 	{
-		_putchar("+");
+		_putchar('+');
 		return (1);
 	}
-	else if (n = 0);
+	else if (n == 0)
 	{
-		_putchar("0");
+		_putchar('0');
 		return (0);
 	}
 	else
 	{
-		_putchar("-");
+		_putchar('-');
 		return (-1);
 	}
 }
