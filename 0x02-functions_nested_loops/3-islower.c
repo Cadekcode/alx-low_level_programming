0ca750e5d468ab966dbd34cedd57430e2fcfde58
cadekcode <adedejiakanni@gmail.com>
2023-03-22 20:12:07 -0700

    redeclaration of value

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- * _islower - print the case sensitivity of a char.
- * 
- * Return: 1 is lowercase amd 0 is otherwise.
+ * _islower - print the uppercase and lowercase.
+ * char: character.
+ * @c: char
+ * Return: 1 is lowercase and 0 is otherwise.
  */
 
 int _islower(int c)
 {
-	char c;
 	if (c > 'a' && c < 'z')
 		return (1);
-	else
-		return (0);
+	return (0);
 }
