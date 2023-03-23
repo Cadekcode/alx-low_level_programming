24df3b59f2d478f74ed5cc55ca6c9b9ab9454e87
cadekcode <adedejiakanni@gmail.com>
2023-03-22 19:35:35 -0700

    always follow compliers errorss

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,20 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - print alphabet and multiply by 10.
- *
+ * print_alphabet_x10 - Prints 10 times the alphabet.
  */
 
 void print_alphabet_x10(void)
 {
-	int counter = 0;
+	int count = 0;
 	char letter;
 
-	while (counter++ >= 9);
-        for (letter ='a'; letter <= 'z'; letter ++);                                                        _putchar(letter);
-        _putchar("\n");	
+	while (count++ <= 9)
+	{
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+		_putchar('\n');
+	}
+	}
 }
