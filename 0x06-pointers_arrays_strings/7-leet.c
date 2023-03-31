7af06edebcd0734617552c8f359bca39d332bd6d
cadekcode <adedejiakanni@gmail.com>
2023-03-30 19:36:25 -0700

    Leetcode for pointers

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: string to be encoded
+ *
+ * Return: pointer to the encoded string
+ */
+
+char *leet(char *s)
+{
+    int i, j;
+    char map[] = "aAeEoOtTlL";
+    char code[] = "4433007711";
+
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        for (j = 0; map[j] != '\0'; j++)
+        {
+            if (s[i] == map[j])
+            {
+                s[i] = code[j];
+            }
+        }
+    }
+
+    return s;
+}
+
