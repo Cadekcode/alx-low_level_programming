d46232b128a1c688c23377d764cf06641de733bf
cadekcode <adedejiakanni@gmail.com>
2023-04-05 18:26:50 -0700

    factorial solution

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -11,13 +11,13 @@ int factorial(int n)
 {
 	int u;
 
-	if (n == 0)
-	{
-		return (0);
-	}
-	else if (n < 0)
+	if (n < 0)
 	{
 		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (0);
 	}
 	else
 	{
