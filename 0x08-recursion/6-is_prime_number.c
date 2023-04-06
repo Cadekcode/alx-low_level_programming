b23386f99e2f5ae042eb457f7ccbd324b01bdd09
cadekcode <adedejiakanni@gmail.com>
2023-04-06 00:20:56 -0700

    prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,15 +9,28 @@
 
 int is_prime_number(int n)
 {
-    if (n <= 1)
+	if (n <= 1)
 	{
-	    return 0;
+	return (0);
 	}
-    for (int i = 2; i <= sqrt(n); i++)
-    {
-	    if (n % i == 0)
-		return 0;
-    }
-    	return 1;
+	if (n <= 3)
+	{
+	return (1);
+	}
+	if (n % 2 == 0 || n % 3 == 0)
+	{
+	return (0);
+	}
+	int i = 5;
+
+	{
+	while (i * i <= n;)
+	{
+if (n % i == 0 || n % (i + 2) == 0)
+	return (0);
+	i += 6;
+	}
+	}
+	return (1);
 }
 
