892f886e5b33ab2a3e61d037f8b2420d316660fb
cadekcode <adedejiakanni@gmail.com>
2023-04-27 20:50:06 -0700

    singly linked files

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * myStartupFun - Apply the constructor attribute to myStartupFun() so that it
+ *   is executed before main()
+ */
+
+void myStartupFun(void) __attribute__ ((constructor));
+
+/**
+ * myStartupFun - implementation of myStartupFun.
+ * Return: 0 is successful, -1 if not
+ */
+
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+	return (0);
+}
